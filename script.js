b1c7e71a0c1f0a90983bf211626e9237db29e5eb
BrennoVilela <brenno.cauet@gmail.com>
2024-07-17 22:26:51 -0300

    commit finalizando o curso da api do rick and morty

---
+++ script.js
@@ -0,0 +1,28 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('#botao');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especieDoPersonagem = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especieDoPersonagem.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
