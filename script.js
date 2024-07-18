b57c1227ac2fee083b0383a3b90ae8eb2517d415
BrennoVilela <brenno.cauet@gmail.com>
2024-07-17 23:42:46 -0300

    colocando 3 imagens

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const imagem = document.querySelector('img');
+const imagem = document.querySelector('#imagem');
+const imagem2 = document.querySelector('#imagem2');
+const imagem3 = document.querySelector('#imagem3');
 const botao = document.querySelector('#botao');
 const nomeDoPersonagem = document.querySelector('#nome');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
 const especieDoPersonagem = document.querySelector('#especie');
+const especieDoPersonagem2 = document.querySelector('#especie2');
+const especieDoPersonagem3 = document.querySelector('#especie3');
 const condicao = document.querySelector('#status');
+const condicao2 = document.querySelector('#status2');
+const condicao3 = document.querySelector('#status3');
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
@@ -18,10 +26,18 @@ pegarPersonagem = () => {
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
+        imagem2.src = data.image;
+        imagem3.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
         especieDoPersonagem.innerHTML = data.species;
+        especieDoPersonagem2.innerHTML = data.species;
+        especieDoPersonagem3.innerHTML = data.species;
         condicao.innerHTML = data.status;
+        condicao2.innerHTML = data.status;
+        condicao3.innerHTML = data.status;
     });
 }
 
